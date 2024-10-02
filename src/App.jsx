7e59fcb37095ae3a2d556633f5711d2f36eb16fc
Sonu Singh <sonusinghrajput270@gmail.com>
2024-10-02 17:50:32 +0530

    Showing User Data

--- src/App.jsx
+++ src/App.jsx
@@ -3,12 +3,13 @@ import { useSelector } from "react-redux";
 
 const App = () => {
 
- const d = useSelector((state) => state);
- console.log(d)
+ const {users} = useSelector((state) => state.userReducer);
   return (
     <div className="p-5">
-      <h1 className="text-center">Learn Reudx part-2</h1>
-     
+      <h1 className="text-center text-2xl bg-red-100 rounded-md mb-3 p-2">User List</h1>
+      <ul className="text-center border-2 border-cyan-600 rounded-md bg-slate-200 flex flex-col gap-2">
+        {users.map((user) => <li className="text-xl font-semibold" key={user.id}>{user.name}</li>)}
+      </ul>
     </div>
   );
 };
